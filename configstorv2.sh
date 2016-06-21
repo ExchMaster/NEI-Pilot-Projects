cf1fea4e4e9b3ee4d6b4ce75a6da49fe7b4fbea3
Jason Ingram <jingram@microsoft.com>
2016-06-20 22:16:36 -0400

    Add disks, update permissions

--- configstorv2.sh
+++ configstorv2.sh
@@ -1,23 +1,15 @@
 #!/bin/bash 
 
 #Updates system and packages to latest revisions
-#yum update -y
+yum update -y
 
 #Creates Raid 0 array on two disks, /dev/sdc & /dev/sdd
-
 mdadm --create /dev/md127 --level 0 --raid-devices 2 /dev/sdc /dev/sdd
-#sleep 10
-
-#Creates partion on Raid 0 volume
-
-#parted /dev/md127 --script mklabel gpt
-#parted /dev/md127 --script mkpart primary 0% 100%
-
 #Formates partition using XFS
 mkfs.xfs /dev/md127 -f
-
 #Creates mount point and updates fstab so mount point persists during reboots, automatically mounts volumes for current session
 mkdir -p /data
 #add to fstabl
 echo /dev/md127 /data                   xfs     defaults        0 0 >> /etc/fstab
 mount -a
+chmod 777 /data
