fb5316f47e145eb53f7d24385aec54182ade43f6
Jason Ingram <jingram@microsoft.com>
2016-06-20 20:56:18 -0400

    Simplify Script

--- configstorv2.sh
+++ configstorv2.sh
@@ -6,18 +6,18 @@
 #Creates Raid 0 array on two disks, /dev/sdc & /dev/sdd
 
 mdadm --create /dev/md127 --level 0 --raid-devices 2 /dev/sdc /dev/sdd
-sleep 10
+#sleep 10
 
 #Creates partion on Raid 0 volume
 
-parted /dev/md127 --script mklabel gpt
-parted /dev/md127 --script mkpart primary 0% 100%
+#parted /dev/md127 --script mklabel gpt
+#parted /dev/md127 --script mkpart primary 0% 100%
 
 #Formates partition using XFS
-mkfs.xfs /dev/md127p1
+mkfs.xfs /dev/md127
 
 #Creates mount point and updates fstab so mount point persists during reboots, automatically mounts volumes for current session
 mkdir -p /data
 #add to fstabl
-echo /dev/md127p1 /data                   xfs     defaults        0 0 >> /etc/fstab
+echo /dev/md127 /data                   xfs     defaults        0 0 >> /etc/fstab
 mount -a
